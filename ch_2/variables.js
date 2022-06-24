dc6a73f25198e7decf0b7630f997cb7111c60f81
Shuvo Khan <agonysuvo@gmail.com>
2022-06-24 19:50:18 +0600

    24 jun

---
+++ ch_2/variables.js
@@ -0,0 +1,39 @@
+var name = 'Shuvo kahn'
+var age = 'I am 24 years old .'
+
+
+//math- operation 
+var num1 = '16'
+var num2 = '8'
+var num3 = '16.44'
+var num4 = '8.66'
+var add = num1 + num2
+    //Remenber for addition alwasys pass value from the parseInt , for float parseFlt
+var add1 = parseInt(num1) + parseInt(num2);
+var add2 = parseFloat(num3) + parseFloat(num4);
+var sub = num1 - num2
+var mul = num1 * num2
+var div = num1 / num2
+
+
+
+console.log('Addition of two number is ' + add)
+console.log('Addition of two number is ' + add1)
+console.log('Addition of two Float number is ' + add2)
+console.log('Subtitution of two number is ' + sub)
+console.log('Multipication of two number is ' + mul)
+console.log('Division  of two number is ' + div)
+
+
+let numl1 = '36'
+let numl2 = '36'
+
+let subl = numl1 - numl2
+console.log('Subtitution of two number is ' + subl)
+
+
+// Using two variable print same line and also break line 
+console.log(name, age, '\nfuck you ')
+
+console.log('Assalamu Alaikum ... How are you ...')
+console.log('he is 23 years old ')
