9fe0f90fc944ae438b6c80b8478efbd4d254628d
Shuvo Khan <agonysuvo@gmail.com>
2022-09-13 21:55:51 +0600

    array
    14 day later ....

---
+++ ch_6/array.js
@@ -0,0 +1,23 @@
+ var arr = [1, 2, 3, 4, 5]
+
+ arr[6] = 7
+
+ console.log(arr[1]);
+ console.log(arr[3]);
+ console.log(arr[6]);
+ console.log(arr[5]);
+
+ console.log(arr);
+
+ console.log(arr.length);
+
+
+
+ ////
+
+ var arr2 = Array(Shuvo)
+
+
+ console.log(arr2);
+
+ console.log(arr2.length);
