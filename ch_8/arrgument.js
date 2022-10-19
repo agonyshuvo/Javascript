fc9b20341882377299a93b33a2a11d5dbeddff11
Shuvo Khan <agonysuvo@gmail.com>
2022-10-19 22:39:55 +0600

    19 oct

---
+++ ch_8/arrgument.js
@@ -0,0 +1,18 @@
+//  function add(x, y) {
+
+//      var result = x + y
+
+//      console.log(result)
+//  }
+
+//  add(70, 20)
+//  add(700, 55)
+
+
+var arr1 = [3, 6, 8]
+var arr1 = [3, 8, 1]
+var arr1 = [3, 7, 2]
+
+var sum1 = 0
+
+for (var i =0 ; irr <a)
---
+++ ch_8/function.js
@@ -0,0 +1,27 @@
+// to define function  Must use 
+
+//function  Shuvo () {
+
+//}
+
+
+function shuvo() {
+    console.log("fuck you")
+}
+
+
+function add() {
+
+    var a = 50
+    var b = 30
+    console.log(a + b)
+}
+
+
+// call function 
+for (var i = 1; i < 10; i++) {
+    shuvo()
+}
+
+
+add()
